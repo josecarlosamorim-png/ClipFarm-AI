b4df64ca36f2104e6e9c8a81d3805642f6023b1e
josecarlosamorim-png <josecarlosamorim@outlook.pt>
2026-07-30 15:49:49 +0100

    Create extractor.py

---
+++ clipfarm_ai_mvp/clipfarm_ai_mvp/audio/extractor.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from moviepy import VideoFileClip
+
+
+class AudioExtractor:
+
+    def extract(self, video_path: Path):
+
+        output_folder = Path("cache")
+
+        output_folder.mkdir(exist_ok=True)
+
+        audio_path = output_folder / "audio.wav"
+
+        video = VideoFileClip(str(video_path))
+
+        video.audio.write_audiofile(
+            str(audio_path),
+            logger=None
+        )
+
+        video.close()
+
+        return audio_path
