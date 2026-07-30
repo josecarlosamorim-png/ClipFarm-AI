33b8f66d30b7903f184e21219370e76facd56a09
josecarlosamorim-png <josecarlosamorim@outlook.pt>
2026-07-30 15:44:34 +0100

    Update app.py

--- clipfarm_ai_mvp/clipfarm_ai_mvp/app.py
+++ clipfarm_ai_mvp/clipfarm_ai_mvp/app.py
@@ -1,12 +1,35 @@
-from core.orchestrator import Orchestrator
+from pathlib import Path
+import tempfile
+
 import streamlit as st
-st.set_page_config(page_title='ClipFinder AI V3')
-st.title('ClipFinder AI V3')
-st.write('Projeto inicial carregado.')
-video=st.file_uploader('Importar vídeo',type=['mp4','mov','mkv'])
+
+from core.orchestrator import Orchestrator
+
+st.set_page_config(page_title="ClipFinder AI V3")
+
+st.title("ClipFinder AI V3")
+
+st.write("Projeto inicial carregado.")
+
+video = st.file_uploader(
+    "Importar vídeo",
+    type=["mp4", "mov", "mkv"]
+)
+
 if video:
+
     st.success(video.name)
+
     if st.button("Analisar"):
+
+        with tempfile.NamedTemporaryFile(
+            delete=False,
+            suffix=Path(video.name).suffix
+        ) as tmp:
+
+            tmp.write(video.read())
+
+            video_path = tmp.name
 
         orchestrator = Orchestrator()
 
