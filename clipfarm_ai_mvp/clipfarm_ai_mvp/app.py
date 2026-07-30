acf80e3782f6f2e312c1a9ff6becaa8fd5830a80
josecarlosamorim-png <josecarlosamorim@outlook.pt>
2026-07-30 15:43:20 +0100

    Update app.py

--- clipfarm_ai_mvp/clipfarm_ai_mvp/app.py
+++ clipfarm_ai_mvp/clipfarm_ai_mvp/app.py
@@ -1,3 +1,4 @@
+from core.orchestrator import Orchestrator
 import streamlit as st
 st.set_page_config(page_title='ClipFinder AI V3')
 st.title('ClipFinder AI V3')
@@ -5,5 +6,12 @@ st.write('Projeto inicial carregado.')
 video=st.file_uploader('Importar vídeo',type=['mp4','mov','mkv'])
 if video:
     st.success(video.name)
-    if st.button('Analisar'):
-        st.info('Pipeline ainda não implementado nesta versão.')
+    if st.button("Analisar"):
+
+        orchestrator = Orchestrator()
+
+        resultado = orchestrator.process_video(video_path)
+
+        st.success("Pipeline executada!")
+
+        st.json(resultado)
